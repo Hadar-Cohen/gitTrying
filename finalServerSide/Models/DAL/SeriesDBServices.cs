093ecfe822cf1b797a0e1d6c487873eb80718ce9
hadartayar <hadar6611@gmail.com>
2021-06-10 20:06:30 +0300

    trying1

---
+++ finalServerSide/Models/DAL/SeriesDBServices.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Configuration;
+using System.Web.Http;
+using Ex2.Models;
+
+
+namespace Ex2.Models.DAL
+{
+    public class SeriesDBServices
+    {
+      
+        public SeriesDBServices()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // This method creates a connection to the database according to the connectionString name in the web.config 
+        //--------------------------------------------------------------------------------------------------
+        public SqlConnection connect(String conString)
+        {
+
+            // read the connection string from the configuration file
+            string cStr = WebConfigurationManager.ConnectionStrings[conString].ConnectionString;
+            SqlConnection con = new SqlConnection(cStr);
+            con.Open();
+            return con;
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // This method inserts a car to the cars table 
+        //--------------------------------------------------------------------------------------------------
+        public int Insert(Series series)
+        {
+            SqlConnection con;
+            //SqlCommand cmd;
+            int numEffected;
+
+            try
+            {
+                con = connect("DBConnectionString"); // create the connection
+            }
+            catch (Exception exe)
+            {
+               throw (exe);
+            }
+
+            //String cStr = BuildInsertCommand(obj);
+            //cmd = CreateCommand(cStr, con);
+
+            String seriesQuery = "INSERT INTO Series_2021 (id,first_air_date,name,origin_country,original_language,overview,popularity,poster_path) VALUES (@id,@first_air_date,@name,@origin_country,@original_language,@overview,@popularity,@poster_path)";
+
+            using (SqlCommand command = new SqlCommand(seriesQuery, con))
+            {
+                command.Parameters.AddWithValue("@id", series.Id);
+                command.Parameters.AddWithValue("@first_air_date", series.First_air_date);
+                command.Parameters.AddWithValue("@name", series.Name);
+                command.Parameters.AddWithValue("@origin_country", series.Origin_country);
+                command.Parameters.AddWithValue("@original_language", series.Original_language);
+                command.Parameters.AddWithValue("@overview", series.Overview);
+                command.Parameters.AddWithValue("@popularity", series.Popularity);
+                command.Parameters.AddWithValue("@poster_path", series.Poster_path);
+
+                try
+                {
+                    numEffected = command.ExecuteNonQuery(); // execute the command
+                    return numEffected;
+                }
+                catch (SqlException exe)
+                {
+                    if (!(exe.Number == 2627))  //if the row doesnt exists so what is your problem? (throw it)
+                        throw (exe); //so throw what was the problem
+                    return -1; //the row exists allready
+                }
+                finally
+                {
+                    if (con != null)
+                    {
+                        // close the db connection
+                        con.Close();
+                    }
+                }
+            }
+        }
+  
+        //---------------------------------------------------------------------------------
+        // Create the SqlCommand
+        //---------------------------------------------------------------------------------
+        private SqlCommand CreateCommand(String CommandSTR, SqlConnection con)
+        {
+
+            SqlCommand cmd = new SqlCommand(); // create the command object
+
+            cmd.Connection = con;              // assign the connection to the command object
+
+            cmd.CommandText = CommandSTR;      // can be Select, Insert, Update, Delete 
+
+            cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+            cmd.CommandType = System.Data.CommandType.Text; // the type of the command, can also be stored procedure
+
+            return cmd;
+        }
+
+      }
+}
---
+++ finalServerSide/Models/Episode .cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ex2.Models.DAL;
+
+namespace Ex2.Models
+{
+    public class Episode
+    {
+        int episodeId;
+        int seriesId;
+        string seriesName;
+        int seasonNum;
+        string episodeName;
+        string imageURL;
+        string overview;
+        string airDate;
+
+        public int SeriesId { get => seriesId; set => seriesId = value; }
+        public string SeriesName { get => seriesName; set => seriesName = value; }
+        public int SeasonNum { get => seasonNum; set => seasonNum = value; }
+        public string EpisodeName { get => episodeName; set => episodeName = value; }
+        public string ImageURL { get => imageURL; set => imageURL = value; }
+        public string Overview { get => overview; set => overview = value; }
+        public string AirDate { get => airDate; set => airDate = value; }
+        public int EpisodeId { get => episodeId; set => episodeId = value; }
+        
+
+        public Episode()
+        {
+
+        }
+
+        public Episode(int episodeId, string seriesName, int seriesId, int seasonNum, string episodeName, string imageURL, string overview, string airDate)
+        {
+            this.episodeId = episodeId;
+            this.seriesName = seriesName;
+            this.seriesId = seriesId;
+            this.seasonNum = seasonNum;
+            this.episodeName = episodeName;
+            this.imageURL = imageURL;
+            this.overview = overview;
+            this.airDate = airDate;
+        }
+
+        public int Insert()
+        {
+            EpisodeDataServices ds = new EpisodeDataServices();
+            return ds.Insert(this);
+        }
+
+        public List<Episode> Get(string seriesName)
+        {
+            EpisodeDataServices d = new EpisodeDataServices();
+            List<Episode> episodeList = d.GetEpisodes(seriesName);
+            return episodeList;
+        }
+    }
+}
